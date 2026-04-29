e34f5741445d69a7415c229cab139c2041b2cfa5
VensenSibanda <154960392+Ven-Sib@users.noreply.github.com>
2026-04-28 23:16:17 -0500

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict1 = {}
+        for i in strs:
+            x = "".join(sorted(i))
+            if x not in dict1:
+                dict1[x] = [i]
+            else:
+                dict1[x].append(i)
+        return list(dict1.values())
+  
