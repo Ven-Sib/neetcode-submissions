2ad10b6a4d508ad102ee2dcd9e75c6984eb21649
VensenSibanda <154960392+Ven-Sib@users.noreply.github.com>
2026-09-20 14:50:59 -0500

    Update: eating-bananas - submission-1

--- Data Structures & Algorithms/eating-bananas/submission-1.py
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -1,26 +1,20 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        """
-            - initiate 2 pointers (l, r) at 0 and at max pile in piles
-            - set res value to r because we can potentially eat fastest at max of   piles
-            - while l <= r, set mid value to (l+r)//2
-            - calculate the time taken to eat a pile at a certain speed
-            - if the total time is less than h, means we move to the left side of our k values, update res too
-            - else to the right
-        
-        """
-        l, r = 1, max(piles)
+        l = 1
+        r = max(piles)
         res = r
 
         while l <= r:
             k = (l+r)//2
-            totalTime = 0
+            total = 0
             for p in piles:
-                totalTime += math.ceil(p/k)
-            if totalTime <= h:
-                res = k
-                r = k -1
+                total += math.ceil(p/k) 
+            if total > h:
+                l = k+1
             else:
-                l = k + 1
+                r = k-1
+                res = min(res, k)
         return res
 
+            
+            
